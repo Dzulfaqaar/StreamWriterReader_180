a35c5542a9d56aa2e9595aa5e7131133acb8299f
dzulfaqaar <fikri6aul@gmail.com>
2023-06-19 14:56:43 +0700

    membuka file mode membaca

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -26,5 +26,12 @@ int main() {
 	//selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjukan ke sebuah file
+	infile.open("contohfile.txt");
+
+
+
 
 	
