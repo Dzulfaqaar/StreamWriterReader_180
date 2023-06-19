ac7521efe6aceb73038c982ef086dd0f497f05af
dzulfaqaar <fikri6aul@gmail.com>
2023-06-19 14:48:10 +0700

    unlimited lop untuk menulis

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -13,4 +13,14 @@ int main() {
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl; 
 
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "_ ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+	}
+
+
 	
